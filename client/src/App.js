405cd56628ce2723baa18053a823fc4dee720665
Ron Perelman <peron4321@gmail.com>
2022-12-19 10:38:09 +0200

    added unary and server stream

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,42 @@
-import { Component } from 'react';
-import { GreeterClient } from './greet_grpc_web_pb'
-import { HelloRequest, HelloReply, DelayedReply } from './greet_pb';
+import { Component } from "react";
+import { GreeterClient } from "./greet_grpc_web_pb";
+import { unary, serverStreaming } from './util'
 
-const client = new GreeterClient(
-  "http://localhost:8080",
-  null,
-  null
-)
+
+const client = new GreeterClient("http://localhost:8080", null, null);
 
 class App extends Component {
-joinHandler = () => {
-  
-  }
-  
+  joinHandler = async () => {
+    let thing = "";
+    const call = prompt("choose which option", 1);
+    switch (call) {
+      case "1":
+        thing = await unary(client)
+        break;
+      case '2':
+        thing = await serverStreaming(client)
+
+        break;
+
+    }
+    document.getElementById("demo").innerHTML = thing
+  };
 
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>hi</h1>
-      <button
-      onClick={this.joinHandler}> press me</button>
-      </header>
-    </div>
-  );
-}}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>hi</h1>
+          <h2>1. Unary</h2>
+          <h2>2. Server Side Streaming</h2>
+          <h2>3. Client Side Streaming</h2>
+          <h2>4. Both Streaming</h2>
+          <button onClick={this.joinHandler}> press me</button>
+          <p id="demo"></p>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ client/src/util.js
@@ -0,0 +1,38 @@
+import { HelloRequest } from "./greet_pb";
+
+const unary = async (client) => {
+    return await new Promise((resolve, reject) => {
+        let hello = new HelloRequest();
+        hello.setName("User");
+        hello.setGreeting("Hello");
+        client.unary(hello, {}, (err, res) => {
+          if (res == null) {
+            reject(err.message)
+          } else {
+            resolve(res.array[0])
+          }
+        });
+    })   
+}
+
+const serverStreaming = async(client) => {
+    return await new Promise((resolve, reject) => {
+        let hello = new HelloRequest();
+        hello.setName("User");
+        hello.setGreeting("Hello");
+        let stream = client.serverStreaming(hello, {});
+        stream.on("data", (res, err) => {
+            if (res == null) {
+                reject(err.message)
+              } else {
+                console.log(res.array[0]);
+                resolve('nice one')
+              }
+        })
+    })
+}
+
+export{
+    unary,
+    serverStreaming
+}
